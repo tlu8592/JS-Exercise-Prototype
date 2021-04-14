eb1ea401ac42f684c15c100099780893a537c52c
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-04-13 22:59:20 -0400

    All instances passed for Person

--- index.js
+++ index.js
@@ -39,15 +39,36 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  Person.prototype.eat = function(food) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(food);
+    }
+  }
+  Person.prototype.poop = function() {
+    this.stomach = [];
+  }
+  Person.prototype.toString = function() {
+    return `${this.name}, ${this.age}`;
   }
  
- 
+const person1 = new Person('Tony', 28);
+console.log("person1",person1);
+console.log("toString method",person1.toString());
 
-  
-  
-  
+// test .eat(food) method
+// Breakfast
+person1.eat('eggs');
+person1.eat('oatmeal');
+person1.eat('sausages');
+
+// test .poop() method
+person1.poop();
+console.log(person1.stomach);
   
   /*
     TASK 2
